a4df51c786d926848f8db6f0fe601ad3227ae99b
KYAMBADDE MATHIAS KATUMBA <kyamathias@yahoo.co.uk>
2019-01-21 12:32:21 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,5 +1,13 @@
-
-function whileLoop(number) {
+function forLoop(array){
+for (i = 0; i < 25; i++) {
+if(i===1){
+array.push("I am 1 strange loop.");
+} else {
+array.push("I am ${i} strange loop.")
+}
+}
+return array;
+}function whileLoop(number) {
 while (number > 0) {
 console.log(–number);
 }
